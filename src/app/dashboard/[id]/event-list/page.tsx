dc7b611220a094ce25709e73e2d73d9674f62554
ega-erinovian <erinovian69@gmail.com>
2024-12-07 10:54:49 +0700

    feat: add sidebar

---
+++ src/app/dashboard/[id]/event-list/page.tsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "@/components/DashboardLayout";
+
+const EventPage = () => {
+  return (
+    <DashboardLayout>
+      <h1>EventPage</h1>
+    </DashboardLayout>
+  );
+};
+
+export default EventPage;
