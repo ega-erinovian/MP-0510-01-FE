cd620b0d4cb54089a5231f8a5180a6764a757da0
ega-erinovian <erinovian69@gmail.com>
2024-12-11 14:46:11 +0700

    feat: change to react query to get events

--- src/features/dashboard/event/event-list/index.tsx
+++ src/features/dashboard/event/event-list/index.tsx
@@ -2,14 +2,20 @@
 
 import useGetEvents from "@/hooks/event/useGetEvents";
 import EventTable from "./components/EventTable";
+import { Loader } from "@/components/ui/loader";
+import Loading from "@/components/dashboard/Loading";
 
 const EventList = () => {
-  const { data, isLoading } = useGetEvents();
+  const { data, isPending } = useGetEvents();
+
+  if (isPending) {
+    return <Loading text="Table" />;
+  }
 
   return (
     <div className="mx-auto p-8">
       <h1 className="text-9xl mb-8 font-bold">Event List</h1>
-      <EventTable data={data} isLoading={isLoading} />
+      <EventTable data={data} isLoading={isPending} />
     </div>
   );
 };
