97cd7cf4ca9238e049ecdba99581113fcf38e7e6
ega-erinovian <erinovian69@gmail.com>
2024-12-12 10:55:58 +0700

    fix: restructuring app route

--- src/app/dashboard/vouchers/create/page.tsx
+++ src/app/dashboard/vouchers/create/page.tsx
@@ -1,5 +1,6 @@
 import DashboardLayout from "@/components/DashboardLayout";
 import CreateVoucherComponent from "@/features/dashboard/voucher/create-voucher";
+import React from "react";
 
 const CreateVoucher = () => {
   return (
