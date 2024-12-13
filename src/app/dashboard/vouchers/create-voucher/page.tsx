04b2082ab2eab1b4021b1a31727e3c469e06be38
ega-erinovian <erinovian69@gmail.com>
2024-12-13 22:21:07 +0700

    fix: fixed event select input at create voucher form

--- src/app/dashboard/vouchers/create-voucher/page.tsx
+++ src/app/dashboard/vouchers/create-voucher/page.tsx
@@ -1,6 +1,5 @@
 import DashboardLayout from "@/components/DashboardLayout";
 import CreateVoucherComponent from "@/features/dashboard/voucher/CreateVoucherPage";
-import React from "react";
 
 const CreateVoucher = () => {
   return (
